a245d90d520b86206fe5668b98ab525cf22f3768
Sonava <sonava@code.edu.az>
2022-11-20 17:35:42 +0400

    Commit 1

---
+++ Js/App.js
@@ -0,0 +1,11 @@
+let carier = document.querySelector("#karyera");
+function portal() {
+    var css = document.carier.style;
+    css.backgroundColor = '#0056c1';
+    css.color = 'white';
+}
+portal()
+
+function under_line() {
+    
+}
