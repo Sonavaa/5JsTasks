db5cb59c696f599e0d8ff64fc481b126069427cf
Sonava <sonava@code.edu.az>
2022-11-22 01:06:57 +0400

    Commit 2

--- Js/App.js
+++ Js/App.js
@@ -1,11 +1,24 @@
-let carier = document.querySelector("#karyera");
+
 function portal() {
-    var css = document.carier.style;
-    css.backgroundColor = '#0056c1';
-    css.color = 'white';
+    document.getElementById("karyera").style.backgroundColor = "#0056c1";
+    document.getElementById("karyera").style.color = "white"
+   
 }
-portal()
+function smth() {
+    document.getElementById("karyera").style.backgroundColor = "white";
+    document.getElementById("karyera").style.color = "#666"
+   
+}
+ 
 
 function under_line() {
     
+    document.getElementById("nav-left").style.textDecoration = "underline";
+    document.getElementById("nav-left").style.transition = "1s";
+    document.getElementById("nav-left").style.color("#666");
 }
+
+function no_line() {
+    document.getElementById("nav-left").style.textDecoration = "none"
+}
+
