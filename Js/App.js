5cac9686501d3752398d496c4a2cdc7e07b62aa5
Sonava <sonava@code.edu.az>
2022-11-24 11:22:01 +0400

    Commit 3

--- Js/App.js
+++ Js/App.js
@@ -1,35 +1,17 @@
-
-function portal() {
-    document.getElementById("karyera").style.backgroundColor = "#0056c1";
-    document.getElementById("karyera").style.color = "white"
-   
-}
-function smth() {
-    document.getElementById("karyera").style.backgroundColor = "white";
-    document.getElementById("karyera").style.color = "#666"
-   
-}
- 
-
-
+let searchBtn = document.querySelector("#searchBtn")
     
-
-
-function under_line() {
-    let mydiv = document.getElementById("nav-left");
-    let btns = document.getElementsByClassName("a");
-   
-    for (let i = 0; i < array.length; i++) {
-   
-       btns[i].addEventListener("onmouseover", function() {
-   
-           var current = document.getElementsByClassName("active");
-           current[0].className = current[0].className.replace(" active", "");
-           this.className += " active";
-       
-       });
-   
-     
-    }
+searchBtn.onclick = function(e){
+    e.preventDefault();
+    let search = document.querySelector("#search")
+   if( search.style.display == "none"){
+    search.style.display = "block";
+   }
+   else{
+    search.style.display = "none";
+   }
 }
-
+let mark = document.querySelector("#mark")
+mark.onclick = function(){
+    let search = document.querySelector("#search")
+    search.style.display = "none";
+}
